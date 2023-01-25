1d0d9f8cdddddd43bc8e85aa4de23c56a004d86e
Shabir <souba67@gmail.com>
2023-01-26 02:13:36 +0700

    Merge branch 'tugas_15' of /Users/shobir/Developer/Belajar/Altera Trainee/BE/Tugas/Tugas_13-180123/SwaggerApi with conflicts.

--- src/main/java/com/Configure/SwaggerApi/SwaggerApiApplication.java
+++ src/main/java/com/Configure/SwaggerApi/SwaggerApiApplication.java
@@ -2,12 +2,12 @@ package com.Configure.SwaggerApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.Configure.SwaggerApi")
 public class SwaggerApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/Configure/SwaggerApi/adapter/presistence/ProductRepository.java
+++ src/main/java/com/Configure/SwaggerApi/adapter/presistence/ProductRepository.java
@@ -1,7 +1,7 @@
-package com.Configure.SwaggerApi.adapter.presistence;
+package com.Configure.SwaggerApi.repository;
 
 import com.Configure.SwaggerApi.entitiy.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
